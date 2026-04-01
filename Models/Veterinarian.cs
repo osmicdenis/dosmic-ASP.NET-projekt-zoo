43214b4f918031430b8e62369a8cd407b4abceb5
Denis Osmić <dosmic@tvz.hr>
2026-04-01 17:47:29 +0200

    Add files via upload

---
+++ Models/Veterinarian.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_projekt.Models
+{
+    public class Veterinarian
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+    }
+}
