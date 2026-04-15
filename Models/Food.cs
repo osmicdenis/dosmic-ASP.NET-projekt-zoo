7e5b0afcdd635766f194a5c92751a6c825a17135
Denis Osmić <dosmic@tvz.hr>
2026-04-15 11:43:09 +0200

    Delete Models directory

--- Models/Food.cs
+++
@@ -1,8 +0,0 @@
-namespace ASP.NET_projekt.Models
-{
-    public class Food
-    {
-        public int Id { get; set; }
-        public required string Name { get; set; }
-    }
-}
--- Models/Veterinarian.cs
+++
@@ -1,9 +0,0 @@
-namespace ASP.NET_projekt.Models
-{
-    public class Veterinarian
-    {
-        public int Id { get; set; }
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-    }
-}
