8e2059d43aec51b78d0ea1e45bcad02d554875ff
Denis Osmić <dosmic@tvz.hr>
2026-05-07 11:33:27 +0200

    Delete ASP.NET-projekt directory

--- ASP.NET-projekt/Models/Zoo.cs
+++
@@ -1,9 +0,0 @@
-namespace ASP.NET_projekt.Models
-{
-    public class Zoo
-    {
-        public int Id { get; set; }
-        public required string Name { get; set; }
-        public required string Location { get; set; }
-    }
-}
