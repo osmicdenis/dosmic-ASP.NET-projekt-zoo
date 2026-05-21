abcdd7298c338379e84e87f25c854b764d067608
Denis Osmić <dosmic@tvz.hr>
2026-05-21 11:20:13 +0200

    Delete ASP.NET-projekt directory

--- ASP.NET-projekt/ViewModels/ZooMapViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ASP.NET_projekt.ViewModels
-{
-    public class ZooMapViewModel
-    {
-        public required string MapImagePath { get; set; }
-        public required List<ZooMapHotspotViewModel> Hotspots { get; set; }
-    }
-}
